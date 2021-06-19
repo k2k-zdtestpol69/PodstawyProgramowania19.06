9c0ab9808d19baec2fb4d1d6d3e8095824c67ecc
K2K <k2k.adam.t@gmail.com>
2021-06-19 10:02:01 +0200

    Initial commit - operatory inkrementacji

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y;
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x);
+
+        y=x++;
+        System.out.println(y);
+        y=++x;
+        System.out.println(y);
+        System.out.println(++y);
+    }
+}
