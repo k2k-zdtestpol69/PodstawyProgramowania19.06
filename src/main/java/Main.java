767880c76d5af672efd6d9183b2ab6372fea30bc
K2K <k2k.adam.t@gmail.com>
2021-06-19 10:36:05 +0200

    tak jak nazwa branch'a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,33 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 3;
-        int y;
-        System.out.println(x++);
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 3;
+//        int y;
+//        System.out.println(x++);
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        y=x++;
+//        System.out.println(y);
+//        y=++x;
+//        System.out.println(y);
+//        System.out.println(++y);
 
-        y=x++;
-        System.out.println(y);
-        y=++x;
-        System.out.println(y);
-        System.out.println(++y);
+        int x;
+        int dzienTygodnia = 1;
+        switch(dzienTygodnia){
+            case 1 :
+                System.out.println("pierwszy jest poniedziałek");
+                break;
+            case 2 :
+                System.out.println("drugi jest wtorek");
+                break;
+            case 3 :
+                System.out.println("trzecia jest środa");
+                break;
+            default:
+                System.out.println("a to się zawsze uruchomi -default");
+        }
+
     }
 }
