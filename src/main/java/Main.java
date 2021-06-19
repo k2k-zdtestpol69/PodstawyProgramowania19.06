88cb9b85f16ee0f6132a86774c610d11a165f35f
K2K <k2k.adam.t@gmail.com>
2021-06-19 10:49:20 +0200

    tak jak nazwa branch'a - pętle for

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,21 +13,33 @@ public class Main {
 //        System.out.println(y);
 //        System.out.println(++y);
 
-        int x;
-        int dzienTygodnia = 1;
-        switch(dzienTygodnia){
-            case 1 :
-                System.out.println("pierwszy jest poniedziałek");
-                break;
-            case 2 :
-                System.out.println("drugi jest wtorek");
-                break;
-            case 3 :
-                System.out.println("trzecia jest środa");
-                break;
-            default:
-                System.out.println("a to się zawsze uruchomi -default");
+//        int x;
+//        int dzienTygodnia = 1;
+//        switch(dzienTygodnia){
+//            case 1 :
+//                System.out.println("pierwszy jest poniedziałek");
+//                break;
+//            case 2 :
+//                System.out.println("drugi jest wtorek");
+//                break;
+//            case 3 :
+//                System.out.println("trzecia jest środa");
+//                break;
+//            default:
+//                System.out.println("a to się zawsze uruchomi -default");
+//        }
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("wyświetlam indeks i : "+ i);
         }
+
+        for (int j = 1; 7 < 16; j++) {
+            if (j == 7) {
+                break;
+            }
+            System.out.println("wyświetlam indeks j :"+ j);
+        }
+
 
     }
 }
